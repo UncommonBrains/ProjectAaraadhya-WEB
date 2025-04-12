5067e3548528cc4a2c3e414f51a526bb44c90388
Gokul G Bhattathiri <gokulgb2020@gmail.com>
2025-04-12 23:47:33 +0530

    Update index.jsx

--- src/contexts/authContext/index.jsx
+++ src/contexts/authContext/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useContext, useEffect } from "react";
 import { auth } from "../../firebase/firebase"
 import {onAuthStateChanged} from "firebase/auth"
