11f24182fee64e1b01c5a2399d5d271d1b380733
Gokul G Bhattathiri <gokulgb2020@gmail.com>
2025-04-13 20:29:56 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -241,7 +241,7 @@ const Header = () => {
                     className="bg-amber-600 rounded-full w-9 h-9 flex items-center justify-center text-white font-medium focus:outline-none"
                     onClick={handleProfileClick}
                   >
-                    {firstLetter }
+                    {firstLetter ? firstLetter : <User className="w-4 h-4" />}
                   </button>
 
                   {/* Profile Dropdown - ONLY shown on desktop */}
